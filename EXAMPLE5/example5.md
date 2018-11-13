c69f7b0d6b379429a9b245678b84fd9fff60e539
joanna-lewis <joanna.lewis@imperial.ac.uk>
2018-11-13 17:11:06 +0000

    Example 5 mostly working.

---
+++ EXAMPLE5/example5.md
@@ -0,0 +1,2 @@
+## Example 5: Stochastic SIR model 
+
---
+++ EXAMPLE5/example5_app.R
@@ -0,0 +1,45 @@
+teaching <- function(path_code, path_docs, default_time,
+                     title = "odin!",
+                     parameters = NULL, extra = NULL) {
+  model <- odin::odin_(path_code)
+  
+  code <- shiny::tagList(
+    shiny::h2("Code"),
+    shinyAce::aceEditor("code", mode = "r", readOnly = TRUE,
+                        value = odin.ui:::read_text(path_code)))
+  docs <- shiny::includeMarkdown(path_docs)
+  
+  ui <- shiny::shinyUI(
+    shiny::navbarPage(
+      title,
+      id = "odin_ui_navbar",
+      inverse = TRUE,
+      shiny::tabPanel(
+        "Code & documentation",
+        shiny::fluidRow(
+          shiny::column(6, code),
+          shiny::column(6, docs))),
+      shiny::tabPanel(
+        "Run",
+        odin.ui:::mod_model_ui("model", NULL)),
+      shiny::tabPanel(
+        "Parameters",
+        odin.ui:::mod_parameter_input("odin_parameter", NULL))))
+  
+  server <- function(input, output, session) {
+    shiny::callModule(odin.ui:::mod_model_server, "model",
+                      model, default_time, parameters, extra)
+    shiny::callModule(odin.ui:::mod_parameter_server, "odin_parameter",
+                      model, default_time, parameters, extra)
+  }
+  
+  shiny::shinyApp(ui, server)
+  
+}
+
+teaching("example5.R", "example5.md", 300, 
+          extra = list(mean_S = function(x) rowMeans(x$S) ,
+                       mean_I = function(x) rowMeans(x$I) ,
+                       mean_R = function(x) rowMeans(x$R) ,
+                       percent_fade = function(x) 100*rowMeans(x$I == 0)) )
+
