8f9b00d5d963b36fde30b45555203472d3aab078
joanna-lewis <joanna.lewis@imperial.ac.uk>
2018-11-14 16:13:10 +0000

    New time scaling in example 1.

--- example1/example1.R
+++ example1/example1.R
@@ -1,6 +1,7 @@
-r <- user(0.005) # growth rate
+r <- user(0.5) # growth rate
 N0 <- user(5)  # initial population size
 
+dt <- 0.01
 
 ## Deterministic solution; approach to steady state over time
 output(N_exact) <- N0 * exp(r*t)
