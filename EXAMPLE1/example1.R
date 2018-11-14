0cc4676b717effc22899b3f241bb12473a02054d
joanna-lewis <joanna.lewis@imperial.ac.uk>
2018-11-14 12:32:02 +0000

    Minor changes to example 1.

--- EXAMPLE1/example1.R
+++ EXAMPLE1/example1.R
@@ -6,7 +6,5 @@ N0 <- user(5)  # initial population size
 output(N_exact) <- N0 * exp(r*t)
 
 ## set up ODEs
-initial(N) <- N0
-deriv(N) <- r * N
-
-output(N_numeric) <- N
+initial(N_numeric) <- N0
+deriv(N_numeric) <- r * N_numeric
